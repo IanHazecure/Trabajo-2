cd56e4196615b876b6bd7b116167aecb8716077a
IanHazecure <136367549+IanHazecure@users.noreply.github.com>
2024-05-22 20:22:38 -0400

    Update cart.js

--- cart.js
+++ cart.js
@@ -27,13 +27,126 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 
-    const initApp = () => {
-        fetch('cats.json')
-        .then(response => response.json())
-        .then(data => {
-            listCats = data;
-            console.log(listCats);
-            addDataToHTML();
+let products = [
+
+    {
+        "id": 1,
+        "name": "Harry",
+        "description": "Gets mad easily, likes Churus and very sleepy",
+        "image": "https://www.vidavetcare.com/wp-content/uploads/sites/234/2022/04/mixed-breed-cat-breed-info.jpeg"
+    
+    },
+    {
+    
+        "id": 2,
+        "name": "Mr Big",
+        "description": "Curious, playful and surly",
+        "image": "https://images.pexels.com/photos/19902540/pexels-photo-19902540/free-photo-of-norwegian-forest-cat.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    
+    
+    },
+    {
+    
+        "id": 3,
+        "name": "Oreo",
+        "description": "Chill, quite and likes solarbeams",
+        "image": "https://inaturalist-open-data.s3.amazonaws.com/photos/369973353/medium.jpg"
+    
+    
+    },
+    {
+    
+        "id": 4,
+        "name": "Chip",
+        "description": "Adorable, friendly and playful. He is deafs",
+        "image": "https://inaturalist-open-data.s3.amazonaws.com/photos/369973353/medium.jpg"
+    
+    
+    },
+    {
+    
+        "id": 5,
+        "name": "Tunita",
+        "description": "Serial Meower, playful and early risers",
+        "image": "https://inaturalist-open-data.s3.amazonaws.com/photos/369973353/medium.jpg"
+    
+    
+    },
+    {
+    
+        "id": 6,
+        "name": "Susy",
+        "description": "Sociable, sweet and relaxed",
+        "image": "https://inaturalist-open-data.s3.amazonaws.com/photos/369973353/medium.jpg"
+    
+    
+    },
+    {
+    
+        "id": 7,
+        "name": "Dorito",
+        "description": "Restless, playful and very annoying",
+        "image": "https://inaturalist-open-data.s3.amazonaws.com/photos/369973353/medium.jpg"
+    
+    
+    },
+    {
+    
+        "id": 8,
+        "name": "Gumball",
+        "description": "Sleepy, relaxed and very cute. Need a dedicated bed ",
+        "image": "https://inaturalist-open-data.s3.amazonaws.com/photos/369973353/medium.jpg"
+    
+    
+    },
+    {
+    
+        "id": 9,
+        "name": "Pucca",
+        "description": "Insect hunter, energetic and playful ",
+        "image": "https://inaturalist-open-data.s3.amazonaws.com/photos/369973353/medium.jpg"
+    
+    
+    },
+    {
+    
+        "id": 10,
+        "name": "Cocada",
+        "description": "Mysterious, curious and scary",
+        "image": "https://inaturalist-open-data.s3.amazonaws.com/photos/369973353/medium.jpg"
+    
+    
+    },
+    {
+    
+        "id": 11,
+        "name": "Bigotes",
+        "description": "Round, generous and sociable",
+        "image": "https://inaturalist-open-data.s3.amazonaws.com/photos/369973353/medium.jpg"
+    
+    
+    },
+    {
+    
+        "id": 12,
+        "name": "Coliflor",
+        "description": "Meowing, gluttonous, playful and affectionate ",
+        "image": "https://inaturalist-open-data.s3.amazonaws.com/photos/369973353/medium.jpg"
+    
+    
+    }
+    ];
+
+    let ListCards = [];
+    function initApp(){
+        products.forEach((value, key)=>{
+            let newDiv = document.createElement('div');
+            list.appendChild(newDiv);`
+            <img src="${value.image}"/>
+            <div class="name">${value.name}</div>
+            <div class="description">${value.description}</div>
+            <button onclick="addToCart(${value.id})">Add to Cart</button>
+            `
         })
 
     }
